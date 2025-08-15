5a2cb205d7a69e2af3fdda2099570c5ea0faf559
Mkikii <maureenwkarimi@gmail.com>
2025-08-15 10:50:34 +0300

    all test passing

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,40 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name="Dog", breed="Beagle"):
+        self._name = None  
+        self._breed = None
+        self.name = name      
+        self.breed = breed    
+
+    def set_name(self, name):
+        if isinstance(name, str) and 1 <= len(name) <= 25:
+            self._name = name
+        else:
+            print("Name must be string between 1 and 25 characters.")
+            
+    def get_name(self):
+        return self._name
+    
+
+    def set_breed(self, breed):
+        if breed in APPROVED_BREEDS:
+            self._breed = breed
+        else:
+            print("Breed must be in list of approved breeds.")
+
+    def get_breed(self):
+        return self._breed
+
+    name = property(get_name, set_name)
+    breed = property(get_breed, set_breed)
+
+    def __str__(self):
+        return f"{self._name} | {self._breed}"
+
+
+Mendoza = Dog("", "French Bulldog")
+print(Mendoza)
+
+BadDog = Dog("Spike", "Dragon")
+print(BadDog)
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,41 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name="Name", job="Admin"):
+        self._name = None
+        self._job = None
+
+        self.name = name
+        self.job = job
+
+    def get_name(self):
+        return self._name
+    
+    def get_job(self):
+        return self._job
+    
+
+    def set_name(self, name):
+        if (isinstance (name, str) and 1 <= len(name) <= 25):
+            self._name = name.title()
+        else:
+            print( "Name must be string between 1 and 25 characters.")
+
+    def set_job(self, job):
+        if(job in APPROVED_JOBS):
+            self._job = job
+        else:
+            print("Job must be in list of approved jobs.")
+    
+    name = property(get_name, set_name)
+    job = property(get_job, set_job)
+
+
+    def __str__(self):
+        return f'{self._name} | {self._job}'
+
+Ken = Person("Kennedy")
+print(Ken)
+
+Ben = Person("Ben", "Human Resources")
+print(Ben)
